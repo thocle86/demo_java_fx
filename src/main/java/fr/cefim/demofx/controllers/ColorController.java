1fc002a90782640e3e43a9908d86188be9b77a01
Thomas Clement <thocle86@gmail.com>
2022-03-04 11:02:29 +0100

    Refacto des méthodes updateColorFromSlider en une seule méthode

--- src/main/java/fr/cefim/demofx/controllers/ColorController.java
+++ src/main/java/fr/cefim/demofx/controllers/ColorController.java
@@ -15,6 +15,7 @@ public class ColorController implements Initializable {
     private Color color = new Color(0, 0, 0);
     private final double MIN_SLIDER = color.getMinRgb();
     private final double MAX_SLIDER = color.getMaxRgb();
+    enum COLOR{RED, GREEN, BLUE};
 
     @FXML
     private Slider sliderRed;
@@ -44,60 +45,48 @@ public class ColorController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
         initParamSliders(MIN_SLIDER, MAX_SLIDER, true, true, 100);
-        initValueSliders();
-        initValueTextFields();
-        
+        updateValueSliders();
+        updateValueTextFields();
+
         updatePaneColor();
+
+        updateColorFromSliders();
 
         updateColorRedFromTextField();
         updateColorGreenFromTextField();
         updateColorBlueFromTextField();
-
-        updateColorRedFromSlider();
-        updateColorGreenFromSlider();
-        updateColorBlueFromSlider();
-
+        
         updateColorFromTextFieldHex();
     }
 
-    // <<< UPDATE COLOR FROM SLIDER___start >>>//
-    private void updateColorRedFromSlider() {
-        sliderRed.valueProperty().addListener((observableValue, oldNumber, newNumber) -> {
-            color.setRed((int) sliderRed.getValue());
-            initValueSliders();
-            initValueTextFields();
-            updatePaneColor();
-            System.out.println(color);
-        });
+    // <<< UPDATE COLOR FROM SLIDERS___start >>>//
+    private void updateColorFromSliders() {
+        String red;
+        sliderRed.valueProperty().addListener((observableValue, oldNumber, newNumber) -> setColor(COLOR.RED));
+        sliderGreen.valueProperty().addListener((observableValue, oldNumber, newNumber) -> setColor(COLOR.GREEN));
+        sliderBlue.valueProperty().addListener((observableValue, oldNumber, newNumber) -> setColor(COLOR.BLUE));
+
     }
 
-    private void updateColorGreenFromSlider() {
-        sliderGreen.valueProperty().addListener((observableValue, oldNumber, newNumber) -> {
-            color.setGreen((int) sliderGreen.getValue());
-            initValueSliders();
-            initValueTextFields();
-            updatePaneColor();
-            System.out.println(color);
-        });
+    private void setColor(COLOR rgb) {
+        switch (rgb) {
+            case RED -> color.setRed((int) sliderRed.getValue());
+            case GREEN -> color.setGreen((int) sliderGreen.getValue());
+            case BLUE -> color.setBlue((int) sliderBlue.getValue());
+        }
+        updateValueSliders();
+        updateValueTextFields();
+        updatePaneColor();
+        System.out.println(color);
     }
-
-    private void updateColorBlueFromSlider() {
-        sliderBlue.valueProperty().addListener((observableValue, oldNumber, newNumber) -> {
-            color.setBlue((int) sliderBlue.getValue());
-            initValueSliders();
-            initValueTextFields();
-            updatePaneColor();
-            System.out.println(color);
-        });
-    }
-    // <<< UPDATE COLOR FROM SLIDER___end >>>//
+    // <<< UPDATE COLOR FROM SLIDERS___end >>>//
 
     // <<< UPDATE COLOR FROM TEXT_FIELD_RGB___start >>>//
     private void updateColorRedFromTextField() {
         textFieldRed.setOnAction(actionEvent -> {
             color.setRed(Integer.parseInt(textFieldRed.getText()));
-            initValueSliders();
-            initValueTextFields();
+            updateValueSliders();
+            updateValueTextFields();
             updatePaneColor();
             System.out.println(color);
         });
@@ -106,8 +95,8 @@ public class ColorController implements Initializable {
     private void updateColorGreenFromTextField() {
         textFieldGreen.setOnAction(actionEvent -> {
             color.setGreen(Integer.parseInt(textFieldGreen.getText()));
-            initValueSliders();
-            initValueTextFields();
+            updateValueSliders();
+            updateValueTextFields();
             updatePaneColor();
             System.out.println(color);
         });
@@ -116,8 +105,8 @@ public class ColorController implements Initializable {
     private void updateColorBlueFromTextField() {
         textFieldBlue.setOnAction(actionEvent -> {
             color.setBlue(Integer.parseInt(textFieldBlue.getText()));
-            initValueSliders();
-            initValueTextFields();
+            updateValueSliders();
+            updateValueTextFields();
             updatePaneColor();
             System.out.println(color);
         });
@@ -128,8 +117,8 @@ public class ColorController implements Initializable {
     private void updateColorFromTextFieldHex() {
         textFieldHex.setOnAction(actionEvent -> {
             color.setHexValue(textFieldHex.getText());
-            initValueSliders();
-            initValueTextFields();
+            updateValueSliders();
+            updateValueTextFields();
             updatePaneColor();
             System.out.println(color);
         });
@@ -137,17 +126,21 @@ public class ColorController implements Initializable {
     // <<< UPDATE COLOR FROM TEXT_FIELD_HEX___end >>>//
 
     // <<< INIT___start >>>//
-    private void initValueTextFields() {
+    private void updateValueTextFields() {
         textFieldRed.setText(String.valueOf(color.getRed()));
         textFieldGreen.setText(String.valueOf(color.getGreen()));
         textFieldBlue.setText(String.valueOf(color.getBlue()));
         textFieldHex.setText(String.valueOf(color.getHexValue()));
     }
 
-    private void initValueSliders() {
+    private void updateValueSliders() {
         sliderRed.setValue(color.getRed());
         sliderGreen.setValue(color.getGreen());
         sliderBlue.setValue(color.getBlue());
+    }
+
+    private void updatePaneColor() {
+        paneColor.setStyle("-fx-background-color: " + color.getHexValue());
     }
 
     private void initParamSliders(double miniSlider, double maxiSlider, boolean tickLabels, boolean tickMarks, int blockIncrement) {
@@ -166,10 +159,6 @@ public class ColorController implements Initializable {
         sliderBlue.setShowTickLabels(tickLabels);
         sliderBlue.setShowTickMarks(tickMarks);
         sliderBlue.setBlockIncrement(blockIncrement);
-    }
-
-    private void updatePaneColor() {
-        paneColor.setStyle("-fx-background-color: " + color.getHexValue());
     }
     // <<< INIT___end >>>//
 }
